11eed3f9db8a01aacda2a0ca5f6e5a1f6efe8d8d
nishant <nishantk045@gmail.com>
2021-06-06 21:18:15 +0530

    Cdk Code

--- my_cdk_app/my_cdk_app_stack.py
+++ my_cdk_app/my_cdk_app_stack.py
@@ -5,6 +5,7 @@ from aws_cdk import core as cdk
 # with examples from the CDK Developer's Guide, which are in the process of
 # being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
+from aws_cdk import aws_s3 as s3
 
 
 class MyCdkAppStack(cdk.Stack):
@@ -12,4 +13,9 @@ class MyCdkAppStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        bucket = s3.Bucket(self,
+                           "MyFirstBucket",
+                           versioned=True,
+                           removal_policy=core.RemovalPolicy.DESTROY,
+                           auto_delete_objects=True
+                           )
